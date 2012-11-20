eb3b9ee8d03cc7becf5276ff036b229e67a40265
Edgar Kalkowski <edgar.kalkowski@uni-kassel.de>
2012-11-20 17:23:12 +0100

    Really animate things and also correctly free colours.

--- src/matrix.c
+++ src/matrix.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 #include <X11/Xlib.h>
+#include <X11/keysym.h>
 
 int main(int argc, char** argv) {
   Display *display = XOpenDisplay(NULL);
@@ -25,7 +27,7 @@ int main(int argc, char** argv) {
     window = DefaultRootWindow(display);
   }
 
-  XSelectInput(display, window, ExposureMask | KeyPressMask | StructureNotifyMask);
+  XSelectInput(display, window, ExposureMask | KeyReleaseMask | StructureNotifyMask);
 
   XWindowAttributes attributes;
   XGetWindowAttributes(display, window, &attributes);
@@ -34,13 +36,19 @@ int main(int argc, char** argv) {
   Atom deleteMessage = XInternAtom(display, "WM_DELETE_WINDOW", 0);
   XSetWMProtocols(display, window, &deleteMessage, 1);
 
-  XEvent event;
-  int killed = 0;
-  XColor darkGreenx, darkGreens, blackx, blacks;
-  XAllocNamedColor(display, DefaultColormapOfScreen(DefaultScreenOfDisplay(display)),
-                   "black", &blacks, &blackx);
-  XAllocNamedColor(display, DefaultColormapOfScreen(DefaultScreenOfDisplay(display)),
-                   "dark green", &darkGreens, &darkGreenx);
+  XColor green;
+  green.red = 0;
+  green.green = 0xFFFF;
+  green.blue = 0;
+
+  XColor black;
+  black.red = 0;
+  black.green = 0;
+  black.blue = 0;
+
+  Colormap cmap = DefaultColormapOfScreen(DefaultScreenOfDisplay(display));
+  XAllocColor(display, cmap, &green);
+  XAllocColor(display, cmap, &black);
 
   GC graphics = XCreateGC(display, window, 0, NULL);
   XSetGraphicsExposures(display, graphics, 0);
@@ -52,53 +60,69 @@ int main(int argc, char** argv) {
     exit(1);
   }
 
+  XEvent event;
   int counter = 0;
+  int killed = 0;
 
   while (!killed) {
     counter++;
     sprintf(text, "Hallo Welt %d", counter);
-    XNextEvent(display, &event);
 
-    switch (event.type) {
-    case ConfigureNotify:
-      XGetWindowAttributes(display, window, &attributes);
-      XFreePixmap(display, buffer);
-      buffer = XCreatePixmap(display, window, attributes.width, attributes.height, attributes.depth);
-      break;
-    case MapNotify:
-    case Expose:;
-      XSetForeground(display, graphics, blackx.pixel);
-      XFillRectangle(display, buffer, graphics, 0, 0, attributes.width, attributes.height);
-      XSetForeground(display, graphics, darkGreens.pixel);
-      XDrawString(display, buffer, graphics, 10, 50, text, strlen(text));
-      XCopyArea(display, buffer, window, graphics, 0, 0, attributes.width, attributes.height, 0, 0);
-      break;
-    case ClientMessage:
+    XSetForeground(display, graphics, black.pixel);
+    XFillRectangle(display, buffer, graphics, 0, 0, attributes.width, attributes.height);
+    XSetForeground(display, graphics, green.pixel);
+    XDrawString(display, buffer, graphics, 10, 50, text, strlen(text));
+    XCopyArea(display, buffer, window, graphics, 0, 0, attributes.width, attributes.height, 0, 0);
+    XSync(display, 0);
 
-      if (event.xclient.data.l[0] == deleteMessage) {
-        killed = 1;
+    if (XEventsQueued(display, &event) > 0) {
+      XNextEvent(display, &event);
+
+      switch (event.type) {
+      case ConfigureNotify:
+        XGetWindowAttributes(display, window, &attributes);
+        XFreePixmap(display, buffer);
+        buffer = XCreatePixmap(display, window, attributes.width, attributes.height, attributes.depth);
+        break;
+      case MapNotify:
+      case Expose:;
+        break;
+      case ClientMessage:
+
+        if (event.xclient.data.l[0] == deleteMessage) {
+          killed = 1;
+        }
+
+        break;
+      case KeyRelease:;
+        KeySym key = XLookupKeysym(&event.xkey, 0);
+
+        if (key == XK_Escape || key == XK_q) {
+          killed = 1;
+        }
+
+        break;
+      default:;
+        char *message = malloc(512*sizeof(char));
+
+        if (message == NULL) {
+          perror("malloc");
+          exit(1);
+        }
+
+        sprintf(message, "Unhandled X event type: %d\n", event.type);
+        fprintf(stderr, message);
+        free(message);
       }
-
-      break;
-    case KeyPress:
-      killed = 1;
-      break;
-    default:;
-      char *message = malloc(512*sizeof(char));
-
-      if (message == NULL) {
-        perror("malloc");
-        exit(1);
-      }
-
-      sprintf(message, "Unhandled X event type: %d\n", event.type);
-      fprintf(stderr, message);
-      free(message);
     }
 
-    usleep(33);
+    usleep(1000);
   }
 
+  free(text);
+  XFreeColors(display, cmap, &black.pixel, 1, 0);
+  XFreeColors(display, cmap, &green.pixel, 1, 0);
+  XFreeGC(display, graphics);
   XCloseDisplay(display);
   return 0;
 }
